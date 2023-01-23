666133c6cfc2d0783b0ba576e904cce41a3057dc
osiel <osiel.oliveira@luizalabs.com>
2023-01-23 15:53:31 -0300

    comit teste variavel number1

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -8,6 +8,12 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+		
+		int number1 = 0;
+		
+		System.out.println(number1);
+		
+	
 	}
-
+	
 }
