b68c377a84dadbc9c55ff1e30f558daf2ec228b7
osiel <osiel.oliveira@luizalabs.com>
2023-01-31 22:06:18 -0300

    aula 108 mapping e join.

--- testedb/src/main/java/com/example/testeDB/entities/Curso.java
+++ testedb/src/main/java/com/example/testeDB/entities/Curso.java
@@ -1,10 +1,16 @@
 package com.example.testeDB.entities;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity //Usado para salvar a entitidade no Banco de dados
@@ -22,6 +28,10 @@ public class Curso {
 
 	public Curso() {
 	}
+	
+	//um  para muitos, inverte a posição e leva em conta a classe e não a variavel
+	@OneToMany(mappedBy = "curso") // Esse recurso mapea o recurso instaciado na outra classe.
+	private List<Aluno> aluno = new ArrayList<>();
 		
 	public Curso(String name, String area) {
 		super();
@@ -48,6 +58,14 @@ public class Curso {
 	}
 	public void setNome(String nome) {
 		this.name = nome;
+	}
+
+	public List<Aluno> getAluno() {
+		return aluno;
+	}
+
+	public void setAluno(List<Aluno> aluno) {
+		this.aluno = aluno;
 	}
 
 	@Override
